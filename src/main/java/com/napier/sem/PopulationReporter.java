877d2a5cbfccff6dddcf057fcc2ee4fce9766b7f
Yenod <74833077+Yenod@users.noreply.github.com>
2026-07-30 20:58:14 +0100

    Added country report method
    [skip actions]

--- src/main/java/com/napier/sem/PopulationReporter.java
+++ src/main/java/com/napier/sem/PopulationReporter.java
@@ -1,6 +1,7 @@
 package com.napier.sem;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class PopulationReporter
 {
@@ -25,7 +26,28 @@ public class PopulationReporter
 
     public void countryReport()
     {
+        String query = "SELECT * FROM country";
 
+        try (PreparedStatement stmt = connection.prepareStatement(query))
+        {
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    Country country = new Country(
+                            rs.getString("Code"),
+                            rs.getString("Name"),
+                            rs.getString("Continent"),
+                            rs.getString("Region"),
+                            rs.getInt("Population"),
+                            rs.getString("Capital")
+                    );
+                    System.out.println(country);
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void cityReport(String capitals)
